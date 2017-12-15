4c5973c291a66bb25480c6d27f56883567a7f212
rkumar-pk <rkumar@prokarma.com>
2017-12-15 14:29:59 +0530

    Cleaned up the code

--- crud-sample/src/main/java/com/prokarma/pkmst/config/AppConfig.java
+++ crud-sample/src/main/java/com/prokarma/pkmst/config/AppConfig.java
@@ -1,9 +1,7 @@
 package com.prokarma.pkmst.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
